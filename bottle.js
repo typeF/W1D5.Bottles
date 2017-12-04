3f9df80298ed4d601def26ae4fab692e6a42427e
Frank Lam <frank.is.missing@gmail.com>
2017-12-04 05:41:49 +0000

    Completed major functions

--- bottle.js
+++ bottle.js
@@ -2,11 +2,36 @@ var money = process.argv[2];
 
 function bottle(money){
 var bottles = (money / 2);
-var caps = 0;
-var empties = 0;
+var total = bottles;
+var caps = bottles;
+var empties = bottles;
+bottles = 0;
 
+while ( (caps >= 4) || (empties >= 2)){
+console.log("hi");
 
-return bottles;
+  if (caps >= 4){
+  var bottlesFromCaps = Math.floor(caps / 4);
+  bottles += bottlesFromCaps;
+  total += bottlesFromCaps;
+  caps -= (4 * bottlesFromCaps);
+  }
+
+  if (empties >= 2){
+  var bottlesFromEmpties = Math.floor(empties / 2);
+  bottles += bottlesFromEmpties;
+  total += bottlesFromEmpties;
+  empties -= (2 * bottlesFromEmpties);
+  }
+
+caps += bottles;
+empties += bottles;
+bottles = 0;
+
+}
+
+console.log("TOTAL BOTTLES: " );
+return
 }
 
 
